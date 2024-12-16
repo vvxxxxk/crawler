234d30fcbe058bfb817f020a2f3a67bbd569eb6a
bumin <vvxxxxk@gmail.com>
2024-12-16 10:47:50 +0900

    Feat: Jasypt 설정 추가 및 암호화 지원

---
+++ src/test/java/com/example/crawler/common/config/JasyptConfigTest.java
@@ -0,0 +1,26 @@
+package com.example.crawler.common.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class JasyptConfigTest extends JasyptConfig {
+
+    @Test
+    public void testEncryptAndDecrypt() {
+        String encrptKey = System.getProperty("jasypt.encryptor.password");
+        String plainText = "test1234";
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword(encrptKey);
+
+        String encryptedText = jasypt.encrypt(plainText);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println("encryptedText = " + encryptedText);
+        System.out.println("decryptedText = " + decryptedText);
+
+        assertThat(plainText).isEqualTo(decryptedText);
+    }
+}
